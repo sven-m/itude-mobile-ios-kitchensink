65d100e5ad903ea50bd65c37d9ca7c73be1c0f83
Pjotter Tommassen <p.tommassen@itude.com>
2012-12-06 15:13:23 +0100

    - refactor all the things \O/

--- itude-mobile-ios-app/Classes/View/Controllers/CustomizedLayout.m
+++ itude-mobile-ios-app/Classes/View/Controllers/CustomizedLayout.m
@@ -14,11 +14,9 @@
 -(void)viewDidLoad {
     [super viewDidLoad];
     MBPanel *myPanel = [self.page firstChildOfKind:[MBPanel class] filterUsingSelector:@selector(name) havingValue:@"myPanel"];
-    UIView *panelView = [myPanel buildViewWithMaxBounds:CGRectZero viewState:self.page.currentViewState]; 
-    panelView.frame = CGRectMake(50,50,200,250);
+    UIView *panelView = [myPanel buildViewWithMaxBounds:CGRectZero forParent:self.view viewState:self.page.currentViewState];
     // instead of hard coding the dimensions you can design the view in interface builder, read the frame and use it here to dimension and position the view with the generated view.
-    [self.view addSubview:panelView];
-    
+    panelView.frame = CGRectMake(50,50,200,250);
 }
 
 @end
